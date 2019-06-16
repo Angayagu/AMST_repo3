c89f4b2b68339fc44754bfa3b4f167b91fbbabd5
Leiber <lvrivas@espol.edu.ec>
2019-06-16 12:34:21 -0500

    Agregado menú
    Menú agregado con las respectivas opciones solicitadas en el TA3

---
+++ app/src/main/java/com/example/adita/amstta3/HomeActivity.java
@@ -0,0 +1,131 @@
+package com.example.adita.amstta3;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        DrawerLayout.DrawerListener {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        MenuItem menuItem = navigationView.getMenu().getItem(0);
+        onNavigationItemSelected(menuItem);
+        menuItem.setChecked(true);
+
+        drawerLayout.addDrawerListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        header.findViewById(R.id.header_title).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, getString(R.string.title_click),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        boolean fragmentTransaction = false;
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_video:
+                fragment = new Fragment_video();
+                fragmentTransaction = true;
+                break;
+            case R.id.nav_calendario:
+                fragment = new Fragment_calendario();
+                fragmentTransaction = true;
+                break;
+            case R.id.nav_mapa:
+                fragment = new Fragment_mapa();
+                fragmentTransaction = true;
+                break;
+            case R.id.nav_grafica:
+                fragment = new Fragment_grafico();
+                fragmentTransaction = true;
+                break;
+            default:
+                throw new IllegalArgumentException("menu option not implemented!!");
+        }
+
+        if(fragmentTransaction) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.home_content, fragment).commit();
+            setTitle(menuItem.getTitle());
+        }
+        else{
+            fragment = HomeContentFragment.newInstance("SELECCIONE UNA OPCIÓN DEL MENÚ");
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.home_content, fragment).commit();
+            setTitle(menuItem.getTitle());
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onDrawerSlide(@NonNull View view, float v) {
+        //cambio en la posición del drawer
+    }
+
+    @Override
+    public void onDrawerOpened(@NonNull View view) {
+        //el drawer se ha abierto completamente
+       // Toast.makeText(this, getString(R.string.navigation_drawer_open),
+              //  Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDrawerClosed(@NonNull View view) {
+        //el drawer se ha cerrado completamente
+    }
+
+    @Override
+    public void onDrawerStateChanged(int i) {
+        //cambio de estado, puede ser STATE_IDLE, STATE_DRAGGING or STATE_SETTLING
+    }
+}
+
